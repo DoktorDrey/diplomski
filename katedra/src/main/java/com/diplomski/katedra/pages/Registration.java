e98a24f94257d1c073172b787507b8d82770ae4f
andrija <andrija@myservername.com>
2015-05-11 14:53:42 +0200

    registration

--- katedra/src/main/java/com/diplomski/katedra/pages/Registration.java
+++ katedra/src/main/java/com/diplomski/katedra/pages/Registration.java
@@ -7,6 +7,7 @@ package com.diplomski.katedra.pages;
 import com.diplomski.katedra.services.app.MainService;
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.beaneditor.Validate;
 import org.apache.tapestry5.ioc.annotations.Inject;
 
 /**
@@ -17,27 +18,31 @@ public class Registration {
 
     @Persist
     @Property
+    @Validate("required")
     private String email;
     @Persist
     @Property
+    @Validate("required")
     private String ime;
     @Persist
     @Property
+    @Validate("required")
     private String prezime;
     @Persist
     @Property
+    @Validate("required")
     private String brIndeksa;
     
     @Inject
     private MainService mainService;
 
     Object onSuccess() {
-
-//        return Pocetna.class;
-        return null;
+        System.out.println("onSuccess");
+        return About.class;
     }
-    
+
     Object onSubmitFromRegistrationForm(){
+        System.out.println("onSubmitFromRegistrationForm");
         /*Class nextPage = null;
         if(mainService.postojiStudent(brIndeksa, ime, prezime)){
             mainService.sendMail(email);
